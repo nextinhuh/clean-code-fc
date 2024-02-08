bdf7ce72d4c6eb0b5cc5b63787a90851201d4691
Álvaro Neto <alvaro_neto96@hotmail.com>
2024-02-08 16:00:12 -0300

    feature: list product e2e tests

--- src/infrastructure/api/__tests__/product.e2e.spec.ts
+++ src/infrastructure/api/__tests__/product.e2e.spec.ts
@@ -30,4 +30,47 @@ describe("E2E test for product", () => {
     expect(response.status).toBe(500);
   });
 
+  it("should list all products", async () => {
+    const response = await request(app)
+      .post("/product")
+      .send({
+        name: "Produto A",
+        price: 50
+      });
+    expect(response.status).toBe(200);
+    const response2 = await request(app)
+      .post("/product")
+      .send({
+        name: "Produto B",
+        price: 100
+      });
+    expect(response2.status).toBe(200);
+
+    const listResponse = await request(app).get("/product").send();
+
+    expect(listResponse.status).toBe(200);
+    expect(listResponse.body.products.length).toBe(2);
+    const product = listResponse.body.products[0];
+    expect(product.name).toBe("Produto A");
+    expect(product.price).toBe(50);
+    const product2 = listResponse.body.products[1];
+    expect(product2.name).toBe("Produto B");
+    expect(product2.price).toBe(100);
+
+    const listResponseXML = await request(app)
+    .get("/product")
+    .set("Accept", "application/xml")
+    .send();
+
+    expect(listResponseXML.status).toBe(200);
+    expect(listResponseXML.text).toContain(`<?xml version="1.0" encoding="UTF-8"?>`);
+    expect(listResponseXML.text).toContain(`<products>`);
+    expect(listResponseXML.text).toContain(`<product>`);
+    expect(listResponseXML.text).toContain(`<name>Produto A</name>`);
+    expect(listResponseXML.text).toContain(`<price>50</price>`);
+    expect(listResponseXML.text).toContain(`<name>Produto B</name>`);
+    expect(listResponseXML.text).toContain(`<price>100</price>`);
+    expect(listResponseXML.text).toContain(`</product>`);
+    expect(listResponseXML.text).toContain(`</products>`);
+  });
 });
